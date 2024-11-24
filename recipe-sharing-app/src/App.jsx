6cd6de7ec467ea714b07af800619081fad8a69e6
Lewis Kimani <lkirigwi@gmail.com>
2024-11-25 00:00:36 +0300

    Fix: Add Router to App.jsx for routing

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import RecipeList from './components/RecipeList';
 import RecipeDetails from './components/RecipeDetails';
 
 const App = () => {
   return (
-    <div>
-      <h1>Recipe Sharing Application</h1>
-      <Routes>
-        <Route path="/" element={<RecipeList />} />
-        <Route path="/recipe/:id" element={<RecipeDetails />} />
-      </Routes>
-    </div>
+    <Router>
+      <div>
+        <h1>Recipe Sharing Application</h1>
+        <Routes>
+          <Route path="/" element={<RecipeList />} />
+          <Route path="/recipe/:id" element={<RecipeDetails />} />
+        </Routes>
+      </div>
+    </Router>
   );
 };
 
