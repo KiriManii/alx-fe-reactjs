3d99a0276bcf02f88e0e66dd87b122f82cf0963e
Lewis Kimani <lkirigwi@gmail.com>
2024-11-25 00:05:41 +0300

    Fix: Add AddRecipeForm to App

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import RecipeList from './components/RecipeList';
 import RecipeDetails from './components/RecipeDetails';
+import AddRecipeForm from './components/AddRecipeForm'; // Import AddRecipeForm
 import SearchBar from './components/SearchBar'; // Import SearchBar
 
 const App = () => {
@@ -10,6 +11,7 @@ const App = () => {
       <div>
         <h1>Recipe Sharing Application</h1>
         <SearchBar /> {/* Display search bar */}
+        <AddRecipeForm /> {/* Display form to add new recipe */}
         <Routes>
           <Route path="/" element={<RecipeList />} />
           <Route path="/recipe/:id" element={<RecipeDetails />} />
